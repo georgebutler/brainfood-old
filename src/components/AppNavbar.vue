19b175e97d2271f6d43ec08013764521e9f37c31
George Butler <georgebutleriv@gmail.com>
2020-03-03 14:00:35 -0800

    Bootstrap variables

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-light">
+    <ul class="navbar-nav mr-auto">
+      <h3>Dashboard</h3>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">John</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <AppNavbar></AppNavbar>
+    <div class="container">
+      <h1>Home</h1>
+      <button class="btn btn-primary">Test Button</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppNavbar from '@/components/AppNavbar'
+
+export default {
+  name: 'Home',
+  components: {
+    AppNavbar
+  }
+}
+</script>
