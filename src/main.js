ce218fda8e417a003d591b905e2d9db60a05d83b
Restyled.io <commits@restyled.io>
2020-03-17 15:56:20 +0000

    Restyled by prettier

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import './assets/scss/app.scss'
+import "./assets/scss/app.scss";
 
-import Vue from 'vue'
-import AllIosIcon from 'vue-ionicons/dist/ionicons-ios.js'
+import Vue from "vue";
+import AllIosIcon from "vue-ionicons/dist/ionicons-ios.js";
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.use(AllIosIcon)
-Vue.config.productionTip = false
+Vue.use(AllIosIcon);
+Vue.config.productionTip = false;
 
-                           new Vue({store, router, render : h => h(App)})
-                               .$mount('#app')
+new Vue({ store, router, render: h => h(App) }).$mount("#app");
