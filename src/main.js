a1af4f27f696878998580792acfa312b838aa4cd
George Butler <georgebutleriv@gmail.com>
2020-03-17 15:23:57 -0700

    Icon fix
    Signed-off-by: George Butler <georgebutleriv@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.config.ignoredElements = [/^ion-/]
 Vue.config.productionTip = false
 
 new Vue({ store, router, render: h => h(App) }).$mount('#app')
