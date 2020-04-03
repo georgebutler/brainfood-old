b3063c4da5c11614666e0c7ba0419b6442b22464
George Butler <georgebutleriv@gmail.com>
2020-04-02 18:32:29 -0700

    Express backend
    Signed-off-by: George Butler <georgebutleriv@gmail.com>

---
+++ server/database.js
@@ -0,0 +1,15 @@
+const debug = require('debug')('brainfood:database')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', function (error) {
+  debug(error)
+})
+
+db.once('open', function () {
+  debug('successfully connected.')
+})
+
+module.exports = db
---
+++ server/models/pantry-item.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const PantryItemSchema = new mongoose.Schema({
+  name: String,
+  pantry: { type: mongoose.Schema.ObjectId, ref: 'Pantry' }
+})
+
+module.exports = mongoose.model('PantryItem', PantryItemSchema)
