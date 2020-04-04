00ba3c06805b0995d295eaab1bc938c9be9691ce
George Butler <georgebutleriv@gmail.com>
2020-04-04 16:36:04 -0700

    tests
    Signed-off-by: George Butler <georgebutleriv@gmail.com>

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 const debug = require('debug')('brainfood:database')
 const mongoose = require('mongoose')
 
-async function connect () {
-  await mongoose.connect(process.env.DB_URI, {
+function connect () {
+  mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
